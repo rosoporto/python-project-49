f2d86b37308f471be2d8a946e148295d2ca3afca
rosoporto <rosoporto@yandex.ru>
2024-02-05 20:20:17 +0300

    add function return

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -3,8 +3,9 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}")
+    return name
 
 
 if __name__ == "__main__":
