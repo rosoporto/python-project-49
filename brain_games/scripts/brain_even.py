7f8b22cfe6910189b74b30b2bfb1e630d724cc1b
rosoporto <rosoporto@yandex.ru>
2024-02-06 22:03:54 +0300

    fix code

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,6 +5,8 @@ from .cli import welcome_user
 
 
 MAX_ROUNDS = 3
+MIN_NUMBER = 1
+MAX_NUMBER = 99
 
 
 def parity_check(number):
@@ -16,7 +18,7 @@ def main():
     name = welcome_user()
     print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
     for i in range(MAX_ROUNDS):
-        random_number = random.randint(1, 99)
+        random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
         print(f"Question: {random_number}")
         answer_user = prompt.string(prompt="Your answer: ")
         verified_number = parity_check(random_number)
